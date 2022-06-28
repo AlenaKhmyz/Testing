60f90fbe1ba281bdc2d92e7c184082b66a93bb8f
Alena <72409651+AlenaKhmyz@users.noreply.github.com>
2022-06-28 19:13:37 +0300

    added react test of rect router dom

--- react-testing/src/App.js
+++ react-testing/src/App.js
@@ -1,30 +1,53 @@
-import logo from './logo.svg';
-import './App.css';
-import { useState, useEffect } from 'react'
+// import logo from './logo.svg';
+// import './App.css';
+// import { useState, useEffect } from 'react'
 
-function App() {
-  const [data, setData] = useState(null)
-  const [toggle, setToggle] = useState(false)
-  const [value, setValue] = useState('')
+// function App() {
+//   const [data, setData] = useState(null)
+//   const [toggle, setToggle] = useState(false)
+//   const [value, setValue] = useState('')
 
-  const onClick = () => setToggle(prev => !prev)
+//   const onClick = () => setToggle(prev => !prev)
 
-  useEffect(() => {
-    setTimeout(() => {
-      setData({})
-    }, 100)
-  }, [])
+//   useEffect(() => {
+//     setTimeout(() => {
+//       setData({})
+//     }, 100)
+//   }, [])
+
+//   return (
+//     <div className="App">
+//       <h1 data-testid="value-elem">{value}</h1>
+//       {toggle === true && <div data-testid='toggle-elem'>toggle</div>}
+//       {data && <div style={{ color: 'blue' }}>data</div>}
+//       <h1>hi</h1>
+//       <button onClick={onClick} data-testid='toggle-btn'>click me!</button>
+//       <input onChange={e => setValue(e.target.value)} type='text' placeholder='input value' />
+//     </div>
+//   );
+// }
+
+// export default App;
+
+import React from 'react'
+import { Routes, Route, Link } from 'react-router-dom'
+import AboutPage from './pages/AboutPage'
+import ErrorPage from './pages/ErrorPage'
+import MainPage from './pages/MainPage'
+
+const App = () => {
 
   return (
-    <div className="App">
-      <h1 data-testid="value-elem">{value}</h1>
-      {toggle === true && <div data-testid='toggle-elem'>toggle</div>}
-      {data && <div style={{ color: 'blue' }}>data</div>}
-      <h1>hi</h1>
-      <button onClick={onClick} data-testid='toggle-btn'>click me!</button>
-      <input onChange={e => setValue(e.target.value)} type='text' placeholder='input value' />
+    <div>
+      <Link to="/" data-testid="main-link">Main</Link>
+      <Link to="/about" data-testid="about-link">About</Link>
+      <Routes>
+        <Route path="/" element={<MainPage/> } />
+        <Route path="/about" element={<AboutPage />} />
+        <Route path="/*" element={<ErrorPage/> } />
+      </Routes>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
--- react-testing/src/App.test.js
+++ react-testing/src/App.test.js
@@ -1,50 +1,48 @@
-import { render, screen, fireEvent, queryByTestId } from '@testing-library/react';
-import userEvent from '@testing-library/user-event'
-import App from './App';
+// import { render, screen, fireEvent, queryByTestId } from '@testing-library/react';
+// import userEvent from '@testing-library/user-event'
+// import App from './App';
 
-describe ('TEST API', () => {
-  test('renders learn react link', () => {
-    render(<App />); //передается компонент, который хотим протестировать
-    const hiElement = screen.getByText(/hi/i);//здесь получаем при помощи объекта screen какой-то элемент
-    const btnElement = screen.getByRole('button');
-    const inputElement = screen.getByPlaceholderText(/input value/i);
-    expect(hiElement).toBeInTheDocument();
-    expect(btnElement).toBeInTheDocument();
-    expect(inputElement).toMatchSnapshot();
-    screen.debug()//помогает с поиском ошибки
-  });
+// describe ('TEST API', () => {
+//   test('renders learn react link', () => {
+//     render(<App />); //передается компонент, который хотим протестировать
+//     const hiElement = screen.getByText(/hi/i);//здесь получаем при помощи объекта screen какой-то элемент
+//     const btnElement = screen.getByRole('button');
+//     const inputElement = screen.getByPlaceholderText(/input value/i);
+//     expect(hiElement).toBeInTheDocument();
+//     expect(btnElement).toBeInTheDocument();
+//     expect(inputElement).toMatchSnapshot();
+//     screen.debug()//помогает с поиском ошибки
+//   });
 
-  test('renders learn react link', async () => {
-    render(<App />);
-    screen.debug()
-    const hiElement = await screen.findByText(/data/i);
-    expect(hiElement).toBeInTheDocument();
-    expect(hiElement).toHaveStyle({color: 'blue'})
-    screen.debug()
-  });
+//   test('renders learn react link', async () => {
+//     render(<App />);
+//     screen.debug()
+//     const hiElement = await screen.findByText(/data/i);
+//     expect(hiElement).toBeInTheDocument();
+//     expect(hiElement).toHaveStyle({color: 'blue'})
+//     screen.debug()
+//   });
 
-  test('click event', () => {
-    render(<App />);
-     const btn = screen.getByTestId('toggle-btn');
-     expect(screen.queryByTestId('toggle-elem')).toBeNull()
-     fireEvent.click(btn)//предназначен для работы с событиями
-     expect(screen.queryByTestId('toggle-elem')).toBeInTheDocument()
-     fireEvent.click(btn)
-     expect(screen.queryByTestId('toggle-elem')).toBeNull()
-  });
+//   test('click event', () => {
+//     render(<App />);
+//      const btn = screen.getByTestId('toggle-btn');
+//      expect(screen.queryByTestId('toggle-elem')).toBeNull()
+//      fireEvent.click(btn)//предназначен для работы с событиями
+//      expect(screen.queryByTestId('toggle-elem')).toBeInTheDocument()
+//      fireEvent.click(btn)
+//      expect(screen.queryByTestId('toggle-elem')).toBeNull()
+//   });
 
-   test('input event', () => {
-    render(<App />);
-     const input = screen.getByPlaceholderText(/input value/i);
-     expect(screen.queryByTestId('value-elem')).toContainHTML('')
+//    test('input event', () => {
+//     render(<App />);
+//      const input = screen.getByPlaceholderText(/input value/i);
+//      expect(screen.queryByTestId('value-elem')).toContainHTML('')
     //  Искусственное событие
     //  fireEvent.input(input, {
     //   target: { value: '123123' }
     //  })
      //Близко к пользователю, обрабатываются события нажатия клавиш и т.д.
-     userEvent.type(input, '123123')
-     expect(screen.queryByTestId('value-elem')).toContainHTML('123123')
-  });
-})
-
-
+//      userEvent.type(input, '123123')
+//      expect(screen.queryByTestId('value-elem')).toContainHTML('123123')
+//   });
+// })
