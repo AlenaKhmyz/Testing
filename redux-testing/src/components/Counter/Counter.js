7b285b9443e0eccb60eea2d768004518154d68ff
Alena <72409651+AlenaKhmyz@users.noreply.github.com>
2022-06-29 21:57:10 +0300

    added new project for  redux testing

---
+++ redux-testing/src/components/Counter/Counter.js
@@ -0,0 +1,24 @@
+import {useDispatch, useSelector} from 'react-redux'
+
+const Counter = () => {
+  const dispatch = useDispatch()
+  const value = useSelector()
+
+  const onIncrement = () => {
+
+  }
+
+  const onDecrement = () => {
+
+  }
+
+  return (
+    <div>
+      <h1>value={value}</h1>
+      <button onClick={onIncrement}>increment</button>
+      <button onClick={onDecrement}>decrement</button>
+    </div>
+  )
+}
+
+export default Counter
