e7875f9c6772d0207c20bb64fb1fff1dd65636f8
Alena <72409651+AlenaKhmyz@users.noreply.github.com>
2022-06-30 11:36:04 +0300

    added integration test

--- redux-testing/src/components/Counter/Counter.js
+++ redux-testing/src/components/Counter/Counter.js
@@ -1,15 +1,17 @@
 import {useDispatch, useSelector} from 'react-redux'
+import { getCounterValue } from '../../store/reducers/selectors/getCounterValue/getCounterValue'
+import {increment, decrement} from '../../store/reducers/counterReducer'
 
 const Counter = () => {
   const dispatch = useDispatch()
-  const value = useSelector()
+  const value = useSelector(getCounterValue)
 
   const onIncrement = () => {
-
+    dispatch(increment())
   }
 
   const onDecrement = () => {
-
+    dispatch(decrement())
   }
 
   return (
